2d33c69b9388605fc2c3f2a82009c939309c406c
Pberu <pberu8531@gmail.com>
2026-04-18 19:55:06 +0300

    Home dashboard page v3:created working cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
 import OrderFilters from "../components/filters/orders.filters";
 import type { FullOrderFilters } from "../types/orders.types";
+import type { MetricItem } from '../components/cards/metrics.cards';
+import MetricsGrid from "../components/cards/metrics.cards";
+import {
+  faUsers,
+  faBox,
+  faShoppingCart,
+  faDollarSign,
+  faTruck,
+  faCheckCircle
+} from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Home() {
@@ -12,11 +22,44 @@ export default function Home() {
     console.log('Filters reset');
   };
 
+  const metricsData: MetricItem[] = [
+    {
+      label: 'Total Customers',
+      value: '3,782',
+      icon: faUsers,
+    },
+    {
+      label: 'Total Orders',
+      value: '5,359',
+      icon: faBox,
+    },
+    {
+      label: 'Pending Deliveries',
+      value: '142',
+      icon: faShoppingCart,
+    },
+    {
+      label: 'Revenue',
+      value: 'KES 1.2M',
+      icon: faDollarSign,
+    },
+    {
+      label: 'Active Agents',
+      value: '24',
+      icon: faTruck,
+    },
+    {
+      label: 'Completed Today',
+      value: '89',
+      icon: faCheckCircle,
+    },
+  ];
+
   return (
     <div>
       <h1>HOMEEEEEE</h1>
       <OrderFilters
-        variant="full"
+        variant="base"
         initialFilters={{
           statuses: ['pending_delivery'],
           startDate: '2024-01-01'
@@ -24,6 +67,8 @@ export default function Home() {
         onFilterChange={handleFilterChange}
         onReset={handleReset}
       />
+
+       <MetricsGrid data={metricsData} />
     </div>
   )
 }
