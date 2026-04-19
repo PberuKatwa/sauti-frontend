d334cfb80ae51d8fe35406856c56d1ef1f6d8e96
Pberu <pberu8531@gmail.com>
2026-04-19 13:33:59 +0300

    Orders Page v2:created order page

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import type { AdminOrderRow, OrderProfile } from "../types/orders.types";
 
 const OrderFallback: AdminOrderRow[] = [
@@ -15,5 +16,21 @@ const OrderFallback: AdminOrderRow[] = [
 export default function Orders() {
 
   const [orders, setOrders] = useState<AdminOrderRow[]>(OrderFallback);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const getAllOrders = async () => {
+    try {
+
+      setLoading(true)
+
+    } catch (error) {
+      console.error(`Error in getting all orders`, error)
+      toast.error(`Error in fetching all orders`)
+    }
+  }
+
 
 }
