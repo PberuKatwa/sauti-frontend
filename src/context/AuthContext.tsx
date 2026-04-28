a36fcc0200742ab0947439bbb7fdefda95cc3eb2
Pberu <pberu8531@gmail.com>
2026-04-28 12:59:40 +0300

    Auth context v2:created provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -7,4 +7,24 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const [user, setUserState] = useState<AuthUser | null>(null);
 
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) setUserState(JSON.parse(storedUser));
+  }, []);
+
+  const setUser = (user: AuthUser | null) => {
+    setUserState(user);
+
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+
+    return (
+      <AuthContext.Provider value={{ user, setUser }}>
+        {children}
+      </AuthContext.Provider>
+    )
+  }
 }
